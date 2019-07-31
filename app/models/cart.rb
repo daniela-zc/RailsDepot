abe40166f4851f56a229de9fb0f603a25d660853
Daniela <daniela.z@stateside.agency>
2019-07-31 16:49:39 -0600

    Task E: A Smarter Cart _ 1

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,18 @@
 class Cart < ApplicationRecord
-    has_many :line_items, dependent: :destroy
+  has_many :line_items, dependent: :destroy
+  
+  def add_product(product)
+    # For logs .. 
+    # logger.debug "add_product !!!!"
+    # logger.debug product[:product][:id]  --> not working as set in the book. Product comes as hash and throws 'method not found' exception
+    product_id = product[:product][:id]
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+
 end
