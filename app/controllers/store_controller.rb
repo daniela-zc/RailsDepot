d02c945513bff7e66a95d5bbbcab58dd8dcb37e3
Daniela <daniela.z@stateside.agency>
2019-08-12 16:17:36 -0600

    Task F: Add a Dash of Ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,13 @@
 class StoreController < ApplicationController
   include Counter
   before_action :set_counter
+
+  include CurrentCart
+  before_action :set_cart
   
   def index
     @products = Product.order(:title)
 
-    @counter_display = nil;
     if @store_counter > 5
       @counter_display = "Store visited #{@store_counter.to_s}  #{"time".pluralize(@store_counter)}"
     end
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -22,9 +22,20 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     follow_redirect!
 
-    assert_select 'h2', 'Your Cart'
-    assert_select 'main td.quantity', "1"
-    assert_select 'main td.title', "Programming Ruby 1.9"
+    assert_select 'article h2', 'Your Cart'
+    assert_select 'article td.quantity', "1"
+    assert_select 'article td', "Programming Ruby 1.9"
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { 
+        product_id: products(:ruby).id }, 
+        xhr: true
+    end
+
+    assert_response :success
+    assert_match /<tr class=\\'line-item-highlight\\'>/, @response.body
   end
   
 
