ef032844d92b3240dc0ee7501cf7228ebbe7d925
Daniela <daniela.z@stateside.agency>
2019-08-12 16:40:56 -0600

    Task F: Destroy cart ajax

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -61,6 +61,7 @@ class CartsController < ApplicationController
     session["cart_id"] = nil
     respond_to do |format|
       format.html { redirect_to store_index_url, notice: 'your cart is currently empty.' }
+      format.js
       format.json { head :no_content }
     end
   end
